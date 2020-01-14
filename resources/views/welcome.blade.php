d38ba841a3676a3bbe638fd2737c3590e7ce5b53
Iñaki-clase <inaki.caballero@ikasle.egibide.org>
2020-01-14 13:54:56 +0100

    Prueba heroku

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,6 +64,7 @@
         </style>
     </head>
     <body>
+    HOLAS
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
