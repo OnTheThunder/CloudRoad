dff8778e6f101e366b450d2f75ef713c56992406
Iñaki-clase <inaki.caballero@ikasle.egibide.org>
2020-01-16 13:33:10 +0100

    ic--> Cambios bootstrap, añadido mer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,13 +1,13 @@
 <!-- Footer -->
-<footer class="text-small mt-5 pt-4">
+<footer class="text-small mt-5 pt-4 ">
     <!-- Footer Links -->
     <div class="container-fluid text-center text-md-left">
         <!-- Copyright -->
-        <div class="footer-copyright text-center py-3">
+        <div class="footer-copyright text-center ">
             <img id="img-footer" src="{{ secure_asset('img/onTheThunder.png') }}" alt="img-equipo"
                  class="img-fluid figure-img">
             <span>© 2020 OnTheThunder: </span>
-            <a href="https://github.com/OnTheThunder/CloudRoad"> Github</a>
+            <a class="" href="https://github.com/OnTheThunder/CloudRoad"> Github</a>
         </div>
     </div>
 </footer>
