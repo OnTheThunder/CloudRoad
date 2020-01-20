c1b6f68751b8ed7c6ae31b7b4bf70b486c178770
Iñaki-clase <inaki.caballero@ikasle.egibide.org>
2020-01-20 14:10:09 +0100

    ic--> Camaras

--- public/js/camaras.js
+++ public/js/camaras.js
@@ -1,11 +1,53 @@
 window.onload = function () {
     $(document).ready(function () {
-        console.log("ready!");
 
+        crearMapa();
+        //url camaras
         let url = "https://www.geo.euskadi.eus/contenidos/ds_localizaciones/camaras_trafico/opendata/camaras-trafico.json";
         var jsonCamaras = getCaramasUrl(url);
-        llenarSelect(jsonCamaras);
+        llenarSelectYMarkers(jsonCamaras);
 
+        /**
+         * LISTENERS
+         */
+        // cuando se clica en una linea de la tabla de lugares
+        $("#tabla-lugares tr").click(function () {
+            // resaltar linea marcada
+            var selected = $(this).hasClass("resaltar");
+            $("#tabla-lugares tr").removeClass("resaltar");
+            if (!selected)
+                $(this).addClass("resaltar");
+            // poner la imagen de la camara
+            let p = this.children;
+            anyadirImagenDeCamara(p[2].textContent);
+        });
+
+        /**
+         * FUNCIONES
+         */
+
+        /**
+         * Funcion que crea el mapa
+         */
+        function crearMapa() {
+            let defaultLatLng = {lat: 42.842326386012516, lng: -2.691612846296414}; //Punto en el que está centrado el mapa por defecto;
+            //Crea el mapa
+            map = new google.maps.Map(document.getElementById('mapa-camaras'), {
+                zoom: 9,
+                minZoom: 9,
+                maxZoom: 14,
+                center: defaultLatLng,
+                options: {
+                    gestureHandling: 'greedy'
+                }
+            });
+        }
+
+        /**
+         * Obtiene de la url dada un json a devolver como objetos
+         * @param url
+         * @returns {string}
+         */
         function getCaramasUrl(url) {
             var matrizCamaras = "";
             $.ajax({
@@ -20,14 +62,72 @@ window.onload = function () {
             return matrizCamaras;
         }
 
-        function llenarSelect(jsonCamaras) {
-            var selector = $('#lugares');
+        /**
+         * Crear los markers de todas las camaras para el mapa
+         * @param jsonCamaras
+         */
+        function llenarSelectYMarkers(jsonCamaras) {
+            var selector = $('#tabla-lugares');
 
             for (let i = 0; i < jsonCamaras.length; i++) {
-                selector.append('<option value=' + jsonCamaras[i].TITLE + '>' + jsonCamaras[i].TITLE + '</option>');
+                //añadir cada lugar y la tabla
+                selector.append('<tr>' +
+                    '<td>' + jsonCamaras[i].TITLE + '</td>' +
+                    '<td>' + jsonCamaras[i].ROAD + '</td>' +
+                    '<td class="d-none">' + jsonCamaras[i].URLCAM + '</td>' +
+                    ' </tr>');
+                // crear marcadores en el mapa con su locaclizacion
+                let lugarIncidencia = {
+                    lat: parseFloat(jsonCamaras[i].LATWGS84),
+                    lng: parseFloat(jsonCamaras[i].LONWGS84)
+                };
+                createMarker(lugarIncidencia, jsonCamaras[i].TITLE, jsonCamaras[i].ROAD, jsonCamaras[i].URLCAM);
             }
         }
 
+        /**
+         * Crear un marker con su infoWindow
+         * @param lugarIncidencia
+         * @param titulo
+         * @param carretera
+         * @param imagen
+         */
+        function createMarker(lugarIncidencia, titulo, carretera, imagen) {
+            let marker = new google.maps.Marker({
+                position: lugarIncidencia,
+                map: map
+            });
+            // la info window que muestra lugar y carretera
+            let infowindow = new google.maps.InfoWindow({
+                content: "<div>" +
+                    "<h4>" + titulo + "</h4>" +
+                    "<span>" + carretera + "</span>" +
+                    "</div>",
+                map: map
+            });
+            marker.addListener('click', function () {
+                map.setZoom(11);
+                infowindow.open(marker.get('map'), marker);
+                map.setCenter(marker.getPosition());
+                // poner imagen de la CAMARA quitando antes la anterior
+                anyadirImagenDeCamara(imagen);
+            });
+            // cerrar las infowindow cuando click en el mapa
+            map.addListener('click', function () {
+                infowindow.close();
+            });
+        }
+
+
+        /**
+         * Funcion para cambiar la imagen de la camara actualmente seleccionada
+         * @param urlImagen
+         */
+        function anyadirImagenDeCamara(urlImagen) {
+            let imagen_caja = $('#imagen');
+            imagen_caja.empty();
+            imagen_caja.append('<img src="' + urlImagen + '">');
+        }
 
     });
 };
--- resources/views/camara/camara.blade.php
+++ resources/views/camara/camara.blade.php
@@ -3,14 +3,34 @@
     <script src="{{asset('js/Camara.js')}}"></script>
     <script src="{{asset('js/camaras.js')}}"></script>
     <h1>Cámaras</h1>
+    Jefe, coordinador y operador ven las camaras
 
     <div class="main">
-        <select id="lugares" name="lugar" class="col">
-            <option value="0">-- Selecciona una opcion</option>
-        </select>
 
-        <div id="mapa">
+        <div class="row border sticky-top">
+            <div id="imagen" class="col-6 bg-white">
+                <!-- Aqui se mete la imagen de la camara por JS dependiendo de la seleccionada -->
+            </div>
 
+            <div id="mapa" class="col-6 bg-white">
+                <div id="mapa-camaras">
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div id="buscador" class="w-100">
+                <table id="tabla-lugares" class="table table-bordered table-hover data-table-container">
+                    <thead class="thead-dark ">
+                    <th>Lugar</th>
+                    <th>Carretera</th>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </table>
+
+            </div>
         </div>
     </div>
 
