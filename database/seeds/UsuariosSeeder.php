56643b071fc510d6c57df91daf0a625e7d23541f
AdrianDanlos <danlosadrian@gmail.com>
2020-01-22 07:50:14 +0100

    .

--- database/seeds/UsuariosSeeder.php
+++ database/seeds/UsuariosSeeder.php
@@ -32,7 +32,7 @@ class UsuariosSeeder extends Seeder
                 ->get();
 
             foreach ($usuario as $user => $value) {
-                DB::table('coordinadores')->insert([
+                DB::table('coordinadors')->insert([
                     'nombre' => $value->nombre,//$faker->firstName,
                     'apellidos' => $faker->lastName,
                     'telefono' => $faker->mobileNumber,
@@ -62,7 +62,7 @@ class UsuariosSeeder extends Seeder
                 ->get();
 
             foreach ($usuario as $user => $value) {
-                DB::table('coordinadores')->insert([
+                DB::table('coordinadors')->insert([
                     'nombre' => $value->nombre,//$faker->firstName,
                     'apellidos' => $faker->lastName,
                     'telefono' => $faker->mobileNumber,
--- resources/views/datos.blade.php
+++ resources/views/datos.blade.php
@@ -2,33 +2,26 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-3">
-                <!--include('')-->
+            <div class="col-md-11">Datos</div>
+            <a class="col-md-1 btn btn-primary">Volver</a>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <select class="form-control" name="selectFiltro" id="selectFiltroDatos">
+                    <option selected="selected" disabled="disabled">Filter</option>
+                    <option>Clientes</option>
+                    <option>Tecnicos</option>
+                    <option>Talleres</option>
+                    <option>Operadores</option>
+                    <option>Coordinadores</option>
+                    <option>Jefes</option>
+                </select>
             </div>
-            <div class="col-md-9">
-                <div class="row">
-                    <div class="col-md-6">Datos</div>
-                    <a class="col-md-6 btn-primary">Volver</a>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <select name="selectFiltro" id="selectFiltroDatos">
-                            <option selected="selected" disabled="disabled">Filter</option>
-                            <option>Clientes</option>
-                            <option>Tecnicos</option>
-                            <option>Talleres</option>
-                            <option>Operadores</option>
-                            <option>Coordinadores</option>
-                            <option>Jefes</option>
-                        </select>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <table class="table table-bordered" id="tabla-datos">
-                        </table>
-                    </div>
-                </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <table class="table table-bordered" id="tabla-datos">
+                </table>
             </div>
         </div>
     </div>
