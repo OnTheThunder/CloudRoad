8f0e1ad2d15cd0b109e2d3dc05e437fb0fef18c4
Iñaki-clase <inaki.caballero@ikasle.egibide.org>
2020-01-27 10:55:10 +0100

    ic--> Cambiar de contraseña

--- resources/views/usuario/password_edit.blade.php
+++ resources/views/usuario/password_edit.blade.php
@@ -5,7 +5,7 @@
         @if(app('request')->input('resultado') == "0")
             <div class="col-12">
                 <div class="alert alert-danger" role="alert">
-                    El email ya existe
+                    Error en el cambio de contraseña
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
@@ -15,7 +15,7 @@
         @if(app('request')->input('resultado') == "1")
             <div class="col-12">
                 <div class="alert alert-success" role="alert">
-                    Usuario agregado correctamente
+                    Contraseña cambiada correctamente
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
@@ -26,30 +26,32 @@
         @include('usuario.aside')
         <div class="col d-flex flex-column mr-2 ">
             <div class="h1 text-center">Cambiar contraseña</div>
-            <div class="row border">
-                <form class="col-12" method="post" action="">
+            <div class="row ">
+                <form class="col-12" method="post" action="{{route('usuario.update')}}">
                     @csrf
 
-                    <div class="col-12 border">
+                    <div class="col-12 ">
                         <div class="form-group">
                             <label for="contra1">Contraseña actual</label>
-                            <input name="contra1" class="form-control" type="password" id="contra1">
+                            <input name="contra1" class="form-control" type="password" id="contra1" required>
                         </div>
                         <div class="form-group">
                             <label for="contra2">Repetir contraseña actual</label>
-                            <input name="contra2" class="form-control" type="password" id="contra2">
+                            <input name="contra2" class="form-control" type="password" id="contra2" required>
+                            <span id='message'></span>
                         </div>
                         <div class="form-group">
                             <label for="nuevaContra">Nueva Contraseña</label>
-                            <input name="nuevaContra" class="form-control" type="password" id="nuevaContra">
+                            <input name="nuevaContra" class="form-control" type="password" id="nuevaContra" required>
                         </div>
                     </div>
                     <div class="row justify-content-center">
-                        <button class="btn btn-primary justify-content-center">Enviar</button>
+                        <button class="btn btn-primary justify-content-center disabled" id="changepassword">Enviar</button>
                     </div>
                 </form>
             </div>
         </div>
     </div>
+    <script src="{{secure_asset('js/password.js')}}"></script>
 
 @endsection
