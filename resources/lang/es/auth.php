b7d5b5db424553117fa4a5122d731c69a001d260
Iñaki-clase <inaki.caballero@ikasle.egibide.org>
2020-01-27 12:44:22 +0100

    ic--> Ahora se puede internacionalizar la app

---
+++ resources/lang/es/auth.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'failed' => 'Algo has puesto mal',
+    'login' => 'Inicio de sesion',
+    'logout' => 'Cerrar sesión',
+];
---
+++ resources/lang/es/general.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'change.password' => 'Cambiar contraseña'
+];
