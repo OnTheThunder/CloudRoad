e1ff307490324002694a9c737d6c98d89b3a7096
AdrianDanlos <danlosadrian@gmail.com>
2020-02-01 16:37:28 +0100

    responsive filters incidencia

--- resources/views/usuario/resto-index.blade.php
+++ resources/views/usuario/resto-index.blade.php
@@ -18,10 +18,10 @@
                 @endif
 
                 <div class="container mb-3">
-                    <h2 class="d-flex justify-content-center p-2">Historial de Incidencias</h2>
+                    <h2 class="d-flex justify-content-center p-2 mt-4 mb-3 page-title">Historial de Incidencias</h2>
                     <div class="row">
-                        <div class="col-md-12 mb-n1 filters-container">
-                            <div class="dropdown show">
+                        <div class="row col-md-12 mb-n1 filters-container pr-0">
+                            <div class="col-12 col-md-6 dropdown show">
                                 <a class="btn btn-primary dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     Filtrar
@@ -47,16 +47,16 @@
                                         </li>
                                     </form>
                                 </ul>
+                                @if(isset($filtro))
+                                    <form class="d-inline-block" action="{{route('main.index')}}" method="get">
+                                        <a href="#" id="btn-filtro-actual" class="btn btn-primary">
+                                            {{ucfirst($filtro)}}
+                                            <button id="cross-remove-filtro"><i class="fas fa-times"></i></button>
+                                        </a>
+                                    </form>
+                                @endif
                             </div>
-                            @if(isset($filtro))
-                                <form action="{{route('main.index')}}" method="get">
-                                    <a href="#" id="btn-filtro-actual" class="btn btn-primary">
-                                        {{ucfirst($filtro)}}
-                                        <button id="cross-remove-filtro"><i class="fas fa-times"></i></button>
-                                    </a>
-                                </form>
-                            @endif
-                            <form class="leyenda-filtro-default mr-1" action="{{route('main.index')}}" method="get">
+                            <form class="col-12 col-md-6 pr-0 d-flex justify-content-end align-items-end leyenda-filtro-default mt-2" action="{{route('main.index')}}" method="get">
                                 <button>
                                     @if(session('orden') == 'reciente' || !session('orden'))
                                         <span>Más Recientes...</span>
